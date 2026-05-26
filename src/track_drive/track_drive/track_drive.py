dd8bbf18065f1486366d537fd5abb9d417663760
JHLee423 <jjunghoo.lee@gmail.com>
2026-05-27 01:35:41 +0900

    commit

--- src/track_drive/track_drive/track_drive.py
+++ src/track_drive/track_drive/track_drive.py
@@ -155,7 +155,7 @@ class SlideWindow:
         # =====================================
         # 2차선 중심 계산
         # =====================================
-        lane_width_offset = 400
+        lane_width_offset = 260
 
         x_location = (
             rightx - lane_width_offset
